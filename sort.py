7616b79e16aa2da9f45f9baf9c3a65794b5a5543
Maaples <105192784+Maaples@users.noreply.github.com>
2022-05-19 15:49:32 +1000

    Starting on main

--- sort.py
+++ sort.py
@@ -10,6 +10,7 @@ class DrawInformation:
     BLUE = 0, 0, 255 
     BACKGROUND = WHITE
     SIDE_PAD = 100
+    TOP_PAD = 150
     
     def __init__(self, width, height, sortList):
         self.width = width
@@ -22,6 +23,43 @@ class DrawInformation:
         
         def set_list(self, sortList):
             self.sortList = sortList
-            self.min = min(sortList)
-            self.max = max(sortList)
-            self.block_width = (self.width - selF.SIDE_PAD) / len(sortList)
+            self.minVal = min(sortList)
+            self.maxVal = max(sortList)
+            self.block_width = round((self.width - selF.SIDE_PAD) / len(sortList))
+            self.block = round((self.height - self.TOP_PAD) / (self.maxVal - self.minVal))
+            
+            self.start_x = self.SIDEPAD // 2
+
+def generateRandomList(numElements, minVal, maxVal):
+    sortList = []
+    
+    for _ in range(numElements):
+        sortList.append(random.randint(minVal, maxVal))
+    return sortList
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    
+    
+    numElements = 50
+    minVal = 0 
+    maxVal = 100
+    
+    sortList = generateRandomList(numElements, minVal, maxVal)
+    drawInfo = DrawInformation(1000, 1000, sortList)
+    
+    while run:
+        clock.tick(60)
+        
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                run == False
+                
+    
+    pygame.quit()
+    
+if __name__ == "__main__":
+    main()
